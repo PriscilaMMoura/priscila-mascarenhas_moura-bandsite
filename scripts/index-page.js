dad3b86f6fe6cf9babbf04a908c63d0c1e16913f
Priscila M. Moura <priscila.moura@gmail.com>
2022-10-21 21:19:03 -0700

    functional requirements for the comments section is met. Still need style.

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,100 +1,62 @@
-/*
- * Html layout
- */
-//Parent - Conversation Blocl
-const convBlock = document.querySelector(".conversation");
-
-//Child
-const convSecHeader = document.createElement("h4");
-convSecHeader.classList.add("conversation__section-header-text");
-convSecHeader.innerText = "Join the Conversation";
-convBlock.appendChild(convSecHeader);
-
-//Child
-const formBlock = document.createElement("div");
-formBlock.classList.add("form");
-formBlock.setAttribute("id", "comments-form");
-convBlock.appendChild(formBlock);
-
-//Grandchild
-const formAvatarCont = document.createElement("div");
-formAvatarCont.classList.add("form__avatar-container");
-formBlock.appendChild(formAvatarCont);
-
-//Great grandchild
-const formAvatar = document.createElement("img");
-formAvatar.classList.add("form__avatar");
-formAvatar.setAttribute("src", "./assets/avatar/Gizmo_29.jpeg");
-formAvatar.setAttribute("alt", "user avatar");
-formAvatarCont.appendChild(formAvatar);
-
-//Grandchild - Form Fields Block
-const formFieldsBlock = document.createElement("form");
-formFieldsBlock.classList.add("form__fields-container");
-formBlock.appendChild(formFieldsBlock);
-
-const formLabelName = document.createElement("label");
-formLabelName.classList.add("form__label-text");
-formLabelName.setAttribute("for", "name");
-formLabelName.innerText = "NAME";
-formFieldsBlock.appendChild(formLabelName);
-
-const formInputName = document.createElement("input");
-formInputName.classList.add("form__field-box");
-formInputName.classList.add("form__field-box--name");
-formInputName.setAttribute("id", "name");
-formInputName.setAttribute("type", "text");
-formInputName.setAttribute("name", "name");
-formInputName.setAttribute("placeholder", "Enter your name");
-formFieldsBlock.appendChild(formInputName);
-
-const formLabelCom = document.createElement("label");
-formLabelCom.classList.add("form__label-text");
-formLabelCom.setAttribute("for", "user-comments");
-formLabelCom.innerText = "COMMENT";
-formFieldsBlock.appendChild(formLabelCom);
-
-const formTextAreaCom = document.createElement("textarea");
-formTextAreaCom.classList.add("form__field-box");
-formTextAreaCom.setAttribute("name", "userComments");
-formTextAreaCom.setAttribute("id", "userComments");
-formTextAreaCom.setAttribute("cols", "30");
-formTextAreaCom.setAttribute("rows", "10");
-formTextAreaCom.setAttribute("placeholder", "Add a new comment");
-formFieldsBlock.appendChild(formTextAreaCom);
-
-const formButton = document.createElement("button");
-formButton.classList.add("form__button");
-formButton.innerText = "COMMENT";
-formFieldsBlock.appendChild(formButton);
-
-//Child
-const commentsBlock = document.createElement("div");
-commentsBlock.classList.add("comments-container");
-convBlock.appendChild(commentsBlock);
-
 const commentsArr = [
   {
     name: "Miles Acosta",
-    timestamps: "12/20/2020",
+    timestamp: "12/20/2020",
     comment:
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
   {
     name: "Emilie Beach",
-    timestamps: "01/09/2021",
+    timestamp: "01/09/2021",
     comment:
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
     name: "Connor Walton",
-    timestamps: "02/17/2021",
+    timestamp: "02/17/2021",
     comment:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
 ];
 
-console.log(commentsArr);
+/*
+ * append array of comments to the page
+ */
+
+const renderTask = (commentObj, tasksListContainer) => {
+  const comments = document.createElement("div");
+  comments.classList.add("comments");
+  tasksListContainer.appendChild(comments);
+
+  const commentsAvatar = document.createElement("div");
+  commentsAvatar.classList.add("comments__avatar-container");
+  comments.appendChild(commentsAvatar);
+
+  const commentsTextBlock = document.createElement("div");
+  commentsTextBlock.classList.add("comments__text-container");
+  comments.appendChild(commentsTextBlock);
+
+  const commentsNameDateBlock = document.createElement("div");
+  commentsNameDateBlock.classList.add("comments__name-date-container");
+  commentsTextBlock.appendChild(commentsNameDateBlock);
+
+  const commentsName = document.createElement("span");
+  commentsName.classList.add("comments__copy-body-text");
+  commentsName.classList.add("comments__copy-body-text--user-name");
+  commentsName.innerText = commentObj.name;
+  commentsNameDateBlock.appendChild(commentsName);
+
+  const commentsDate = document.createElement("span");
+  commentsDate.classList.add("comments__copy-body-text");
+  commentsDate.classList.add("comments__copy-body-text--user-date");
+  commentsDate.innerText = commentObj.timestamp;
+  commentsNameDateBlock.appendChild(commentsDate);
+
+  const commentsContent = document.createElement("span");
+  commentsContent.classList.add("comments__copy-body-text");
+  commentsContent.innerText = commentObj.comment;
+  commentsTextBlock.appendChild(commentsContent);
+};
 
 /**
  * Submit comment event.
@@ -103,19 +65,28 @@ const commentsForm = document.getElementById("comments-form");
 commentsForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const inputBoxName = document.getElementById("name");
+  const inputBoxComments = document.getElementById("userComments");
+
+  if (inputBoxName.value === "" || inputBoxComments.value === "") {
+    alert("error");
+    return;
+  }
+
   const date = new Date();
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
   const currentDate = `${month}/${day}/${year}`;
-
   const userInput = {
     name: event.target.name.value,
-    timestamps: currentDate,
+    timestamp: currentDate,
     comment: event.target.userComments.value,
   };
 
   addNewComment(userInput);
+  inputBoxName.value = "";
+  inputBoxComments.value = "";
 });
 
 /*
@@ -123,6 +94,16 @@ commentsForm.addEventListener("submit", (event) => {
  */
 function addNewComment(userInput) {
   commentsArr.push(userInput);
-  console.log(userInput);
+  appendCommentObj();
 }
-console.log(commentsArr);
+
+const appendCommentObj = () => {
+  // get the tasks container using querySelector
+  const commentsContainer = document.querySelector(".comments-container");
+  commentsContainer.innerHTML = "";
+  for (let i = commentsArr.length - 1; i >= 0; i--) {
+    renderTask(commentsArr[i], commentsContainer);
+  }
+};
+
+appendCommentObj();
